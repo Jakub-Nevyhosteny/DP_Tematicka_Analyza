c1a37b95731b25693b70137e393b1ed8dadf95d4
Jakub-Nevyhosteny <145538596+Jakub-Nevyhosteny@users.noreply.github.com>
2024-04-16 17:53:36 +0200

    Update Topic_classification_Keras_cze.py

--- Klasifikace/Topic_classification_Keras_cze.py
+++ Klasifikace/Topic_classification_Keras_cze.py
@@ -134,7 +134,6 @@ X_train, X_test, y_train, y_test = train_test_split(texts, one_hot_labels, test_
 
 # Trénování
 history = model.fit(X_train, y_train, epochs=10, batch_size=512, validation_data=(X_test, y_test), callbacks=early_stopping)
-#model.fit(X_train, y_train, epochs=2, batch_size=512) #2
 
 
 # Graf přesnosti v průběhu trénování
